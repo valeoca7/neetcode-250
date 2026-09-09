8092b39bcdbca99233346c691001b6bf028dc44d
Val <141705354+valeoca7@users.noreply.github.com>
2026-09-08 21:51:06 -0700

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut left = 0;
+        let mut right = numbers.len() - 1;
+
+        while left < right {
+            let sum = numbers[left] + numbers[right];
+
+            if sum == target {
+                return vec![
+                    (left + 1) as i32,
+                    (right + 1) as i32,
+                ];
+            }
+
+            if sum < target {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+
+        vec![]
+    }
+}
