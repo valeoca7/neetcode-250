9adcb6272f25cb8f9b79e77ae2a4a77ba9589077
Val <141705354+valeoca7@users.noreply.github.com>
2026-09-11 21:37:52 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut min_price = prices[0];
+        let mut max_profit = 0;
+
+        for price in prices {
+            min_price = min_price.min(price);
+
+            let profit = price - min_price;
+            max_profit = max_profit.max(profit);
+        }
+
+        max_profit
+        
+    }
+}
